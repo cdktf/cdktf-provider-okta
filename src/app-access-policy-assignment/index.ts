113ee4c289bf5af7cca6bc1c6ec69e5554544a69
team-tf-cdk <github-team-tf-cdk@hashicorp.com>
2023-10-04 12:38:28 +0000

    chore: self mutation
    Signed-off-by: team-tf-cdk <github-team-tf-cdk@hashicorp.com>

--- src/app-access-policy-assignment/index.ts
+++ src/app-access-policy-assignment/index.ts
@@ -1,3 +1,8 @@
+/**
+ * Copyright (c) HashiCorp, Inc.
+ * SPDX-License-Identifier: MPL-2.0
+ */
+
 // https://registry.terraform.io/providers/okta/okta/4.4.2/docs/resources/app_access_policy_assignment
 // generated from terraform resource schema
 
