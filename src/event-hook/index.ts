799b04b41a61cc88ba74db6aa255ae03bcd9c10f
github-team-tf-cdk <github-team-tf-cdk@hashicorp.com>
2024-03-04 10:00:15 +0000

    chore(deps): update  by cdktf/cdktf-repository-manager@b7c72ed

--- src/event-hook/index.ts
+++ src/event-hook/index.ts
@@ -1,8 +1,3 @@
-/**
- * Copyright (c) HashiCorp, Inc.
- * SPDX-License-Identifier: MPL-2.0
- */
-
 // https://registry.terraform.io/providers/okta/okta/4.8.0/docs/resources/event_hook
 // generated from terraform resource schema
 
