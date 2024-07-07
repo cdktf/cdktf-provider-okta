40214373a2b815514389da85f9b96af26741e455
team-tf-cdk <github-team-tf-cdk@hashicorp.com>
2024-07-07 06:12:42 +0000

    chore: self mutation
    Signed-off-by: team-tf-cdk <github-team-tf-cdk@hashicorp.com>

--- src/data-okta-email-customizations/index.ts
+++ src/data-okta-email-customizations/index.ts
@@ -1,3 +1,8 @@
+/**
+ * Copyright (c) HashiCorp, Inc.
+ * SPDX-License-Identifier: MPL-2.0
+ */
+
 // https://registry.terraform.io/providers/okta/okta/4.9.1/docs/data-sources/email_customizations
 // generated from terraform resource schema
 
