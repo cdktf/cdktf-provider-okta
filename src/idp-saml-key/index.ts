dba7adfac392f36a181b885b25a988466097469d
github-team-tf-cdk <github-team-tf-cdk@hashicorp.com>
2024-10-20 06:09:56 +0000

    chore(deps): update  by cdktf/cdktf-repository-manager@1fb0970

--- src/idp-saml-key/index.ts
+++ src/idp-saml-key/index.ts
@@ -1,8 +1,3 @@
-/**
- * Copyright (c) HashiCorp, Inc.
- * SPDX-License-Identifier: MPL-2.0
- */
-
 // https://registry.terraform.io/providers/okta/okta/4.11.0/docs/resources/idp_saml_key
 // generated from terraform resource schema
 
