ca7c6cdce07d55261670f6d174c2efd3329f124d
github-team-tf-cdk <github-team-tf-cdk@hashicorp.com>
2025-01-23 22:13:21 +0000

    chore(deps): update  by cdktf/cdktf-repository-manager@42ce033

--- src/trusted-origin/index.ts
+++ src/trusted-origin/index.ts
@@ -1,8 +1,3 @@
-/**
- * Copyright (c) HashiCorp, Inc.
- * SPDX-License-Identifier: MPL-2.0
- */
-
 // https://registry.terraform.io/providers/okta/okta/4.13.0/docs/resources/trusted_origin
 // generated from terraform resource schema
 
