bfb565a73c0b49763ede902f6d0920b24faa9e71
team-tf-cdk <github-team-tf-cdk@hashicorp.com>
2025-03-23 06:14:55 +0000

    chore: self mutation
    Signed-off-by: team-tf-cdk <github-team-tf-cdk@hashicorp.com>

--- src/app-oauth-redirect-uri/index.ts
+++ src/app-oauth-redirect-uri/index.ts
@@ -1,3 +1,8 @@
+/**
+ * Copyright (c) HashiCorp, Inc.
+ * SPDX-License-Identifier: MPL-2.0
+ */
+
 // https://registry.terraform.io/providers/okta/okta/4.15.0/docs/resources/app_oauth_redirect_uri
 // generated from terraform resource schema
 
